63c883c7f2542266fb434ffd60bb09b27000a4cf
FennecFoxie <alice.levkovich@gmail.com>
2017-08-20 12:45:06 +0300

    added filename output and prevent sending invalid form data

--- assets/js/master.js
+++ assets/js/master.js
@@ -33,90 +33,91 @@ $(document).ready(function() {
     ]
   });
 
+
+//output filename on upload
+  $("#photo").on("change", function() {
+    var fullPath = $("#photo").val();
+    var filename = fullPath.replace(/^.*[\\\/]/, '');
+    $("#filename").text(filename);
+  });
+
+
+
   $('.feedback-form').validator().on('submit', function(e) {
-      e.preventDefault();
+    if (!e.isDefaultPrevented()) {
+    e.preventDefault();
 
-      proceed = true;
-      var allowed_file_size = "1048576"; //allowed file size
-      var allowed_files = ['image/png', 'image/gif', 'image/jpeg'];
-      //check file size and type before upload, works in modern browsers
-      if (window.File && window.FileReader && window.FileList && window.Blob) {
-        var total_files_size = 0;
-        $(this.elements['photo'].files).each(function(i, ifile) {
-          if (ifile.value !== "") { //continue only if file(s) are selected
-            if (allowed_files.indexOf(ifile.type) === -1) { //check unsupported file
-              alert(ifile.name + " is unsupported file type!");
-              proceed = false;
-            }
-            total_files_size = total_files_size + ifile.size; //add file size to total size
+    proceed = true;
+    var allowed_file_size = "1048576"; //allowed file size
+    var allowed_files = ['image/png', 'image/gif', 'image/jpeg'];
+    //check file size and type before upload, works in modern browsers
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+      var total_files_size = 0;
+      $(this.elements['photo'].files).each(function(i, ifile) {
+        if (ifile.value !== "") { //continue only if file(s) are selected
+          if (allowed_files.indexOf(ifile.type) === -1) { //check unsupported file
+            alert(ifile.name + " is unsupported file type!");
+            proceed = false;
           }
-        });
-        if (total_files_size > allowed_file_size) {
-          alert("Make sure total file size is less than 1 MB!");
-          proceed = false;
+          total_files_size = total_files_size + ifile.size; //add file size to total size
         }
+      });
+      if (total_files_size > allowed_file_size) {
+        alert("Make sure total file size is less than 1 MB!");
+        proceed = false;
       }
+    }
 
-      //if everything's ok, continue with Ajax form submit
-      if (proceed) {
+    //if everything's ok, continue with Ajax form submit
+    if (proceed) {
 
-        var post_url = $(this).attr("action"); //get form action url
-        var request_method = $(this).attr("method"); //get form GET/POST method
-        var form_data = new FormData(this); //constructs key/value pairs representing fields and values
+      var post_url = $(this).attr("action"); //get form action url
+      var request_method = $(this).attr("method"); //get form GET/POST method
+      var form_data = new FormData(this); //constructs key/value pairs representing fields and values
 
 
-        $.ajax({
-          type: request_method,
-          url: post_url,
-          data: form_data,
-          dataType: "json",
-          contentType: false,
-          cache: false,
-          processData: false,
-          // }).done(function(res) { //fetch server "json" messages when done
-          //   alert(res);
-          //   if (res.type == "error") {
-          //   }
-          //   if (res.status == 200) {
-          //     $('.feedback-form__element').each(function() {
-          //       $(this).val("");
-          //     })
-          //     $('.modal').modal('show');
-          //   }
-          // });
-
-        }).always(function(data) {
-            var answer = JSON.stringify(data);
-            $('.js-modal-text').text(data.responseText);
-            $('.feedback-form')[0].reset();
-            $('.modal').modal('show');
-        });
+      $.ajax({
+        type: request_method,
+        url: post_url,
+        data: form_data,
+        dataType: "json",
+        contentType: false,
+        cache: false,
+        processData: false,
+      }).always(function(data) {
+        var answer = JSON.stringify(data);
+        $('.js-modal-text').text(data.responseText);
+        $('.feedback-form')[0].reset();
+        $("#filename").text("");
+        $('.modal').modal('show');
+      });
     } else {
       return false;
     }
+  }
 
   });
 
 
-$('.button--close-modal').on('click', function() {
-  $('.modal').modal('hide');
-});
+  $('.button--close-modal').on('click', function() {
+    $('.modal').modal('hide');
+  });
 
-//smooth scroll to anchor
-$('a[href*="#"]:not([href="#"])').click(function() {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000);
-      return false;
+  //smooth scroll to anchor
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
     }
-  }
-});
+  });
 
-//masking input
-$('.feedback-form__element--phone').mask('+375 (99) 999-99-99');
+  //masking input
+  $('.feedback-form__element--phone').mask('+375 (99) 999-99-99');
 
 });
